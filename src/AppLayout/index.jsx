fafd5cabc674302009e82d914a411fcf57e23009
Marcus Rognes <marcus@rognes.guru>
2017-05-02 21:54:59 +0200

    Added eslint, started to add base layout

---
+++ src/AppLayout/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AppLayout extends Component {
+	render() {
+		return <div
+			className="app-layout"
+		>
+
+		</div>
+	};
+}
+
+export default AppLayout;
