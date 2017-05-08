409491339772df9e27020d75e364b233d1dc020b
Marcus Rognes <marcus@rognes.guru>
2017-05-08 22:33:48 +0200

    Started to add form generator

---
+++ src/Form/index.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './FormStyle.scss';
+
+class Form extends Component {
+	render() {
+		return <div
+			className="form"
+		>
+
+		</div>;
+	}
+}
+
+Form.defaultProps = {
+	schema: [],
+};
+
+export default Form;
